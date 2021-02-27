34cacf08b7b1e5c8ccef878e6ccdd5a6826855eb
unknown <basavarajubc451@gmaol.com>
2021-02-27 15:05:32 +0530

    hi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,71 +1,23 @@
-import { ViewBannerService } from './view-banner.service';
-import { BannerService } from './banner.service';
-import { ImageService } from './image.service';
-import { AddserviceService } from './addservice.service';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HashLocationStrategy, PathLocationStrategy, LocationStrategy } from '@angular/common';
 import {ReactiveFormsModule, FormControl} from '@angular/forms';
-import {HttpModule } from '@angular/http';
 import{FormsModule} from '@angular/forms';
-import { Http, Headers, RequestOptions, Response } from '@angular/http';
+
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppRoutingModule } from './/app-routing.module';
-import { FooterComponent } from './common/footer/footer.component';
-import { HeaderComponent } from './common/header/header.component';
-import { CategoriesComponent } from './categories/categories.component';
-import { ProductsComponent } from './products/products.component';
-import { AddProductsComponent } from './add-products/add-products.component';
-import { CustomersComponent } from './customers/customers.component';
-import { ProductOrdersComponent } from './product-orders/product-orders.component';
-import { AccountComponent } from './account/account.component';
-import { LoginComponent } from './login/login.component';
-import{RouterModule} from '@angular/router';
-import { BannerComponent } from './banner/banner.component';
-import { ImageComponent } from './image/image.component';
-import { ViewBannerComponent } from './view-banner/view-banner.component';
-import { ViewimageComponent } from './viewimage/viewimage.component';
-import { AddComponent } from './add/add.component';
-import { AddImageComponent } from './add-image/add-image.component';
-import { EditImageComponent } from './edit-image/edit-image.component';
-import { CategoryComponent } from './category/category.component';
-import { ViewcategoryComponent } from './viewcategory/viewcategory.component';
-import { UpdatecategoryComponent } from './updatecategory/updatecategory.component';
-import { ViewproductComponent } from './viewproduct/viewproduct.component';
-import { ProductComponent } from './product/product.component';
-import { ListviewproductComponent } from './listviewproduct/listviewproduct.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PipesComponent } from './pipes/pipes.component';
 
 @NgModule({
   declarations: [ 
     AppComponent,
     DashboardComponent,
-    FooterComponent,
-    HeaderComponent,
-    CategoriesComponent,
-    ProductsComponent,
-    AddProductsComponent,
-    CustomersComponent,
-    ProductOrdersComponent,
-    AccountComponent,
-    LoginComponent,
-    BannerComponent,
-
-    ImageComponent,
-    ViewBannerComponent,
-    AddProductsComponent,
-    ViewimageComponent,
-    AddComponent,
-    AddImageComponent,
-    EditImageComponent,
-    CategoryComponent,
-    ViewcategoryComponent,
-    UpdatecategoryComponent,
-    ViewproductComponent,
-    ProductComponent,
-    ListviewproductComponent,
+    PipesComponent
     
     
   
@@ -74,14 +26,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpModule,
+    
     ReactiveFormsModule,
     BrowserAnimationsModule,
     
     
     
   ],
-  providers: [AddserviceService,ImageService,BannerService,ViewBannerService,
+  providers: [
     { provide: LocationStrategy, useClass: HashLocationStrategy },
   ], 
   bootstrap: [AppComponent]
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,165 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  ViewOrderItemList:any={};
+  OrderItemList:any=[
+    {
+      Id:1,
+    customerName:"Basavaraju",
+    NoOfOrders:2,
+    TotalAmount:500,
+    Status:"OrderReceived",
+    Items:[
+      {
+      itemName:'Pizza',
+      price:250,
+      qty:1
+    },{
+      itemName:'Coke',
+      price:250,
+      qty:1
+    }
+  ],
+  address:"Banashankari 3rd stage, Bangalore"
 
-  constructor() { }
+  },
+  {
+    Id:2,
+    customerName:"Kedar",
+    NoOfOrders:1,
+    TotalAmount:250,
+    Status:"OrderReceived",
+    Items:[
+      {
+      itemName:'Pizza',
+      price:250,
+      qty:1
+    }
+  ],
+  address:"Jayanagar 3rd stage, Bangalore"
 
-  ngOnInit() {
+  },
+  {
+    Id:3,
+    customerName:"Ramu",
+    NoOfOrders:3,
+    TotalAmount:600,
+    Status:"Preparing",
+    Items:[
+      {
+      itemName:'Pizza',
+      price:250,
+      qty:1
+    },{
+      itemName:'Coke',
+      price:250
+    },{
+      itemName:'FrenchFries',
+      price:100,
+      qty:1
+    }
+  ],
+  address:"Banashankari 3rd stage, Bangalore"
+
+  },
+  {
+    Id:4,
+    customerName:"Shamu",
+    NoOfOrders:2,
+    TotalAmount:400,
+    Status:"ReadyToServe",
+    Items:[
+      {
+      itemName:'Pizza',
+      price:250,
+      qty:1
+    },{
+      itemName:'FrenchFries',
+      price:150,
+      qty:1
+    }
+  ],
+  address:"Banashankari 3rd stage, Bangalore"
+
+  },
+  {
+    Id:5,
+    customerName:"Raju",
+    NoOfOrders:1,
+    TotalAmount:100,
+    Status:"OrderReceived",
+    Items:[
+     {
+      itemName:'FrenchFries',
+      price:100,
+      qty:1
+    }
+  ],
+  address:"Banashankari 3rd stage, Bangalore"
+
   }
 
+
+];
+orderrxvd:number;
+preparing:number;
+readytoserve:number;
+totalamount:number;
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+this.checkvalue();
+  }
+checkvalue(){
+  let result= this.OrderItemList.filter(data => data.Status =="OrderReceived");
+let result1= this.OrderItemList.filter(data => data.Status =="Preparing");
+let result2= this.OrderItemList.filter(data => data.Status =="ReadyToServe");
+
+this.orderrxvd=result.length;
+this.preparing=result1.length;
+this.readytoserve=result2.length;
+
+this.totalamount= result2.reduce(function(prev, cur) {
+  return prev + cur.TotalAmount;
+}, 0);
+
+}
+
+  checkstatus(data,id){
+console.log(data,id);
+if(data=="OrderReceived"){
+  this.OrderItemList.forEach(element => {
+    if(element.Id==id){
+element.Status="Preparing";
+this.checkvalue();
+// this.router.navigate(['/viewproduct']);
+
+    }
+  });
+  
+}else if(data=="Preparing"){
+  this.OrderItemList.forEach(element => {
+    if(element.Id==id){
+element.Status="ReadyToServe";
+this.checkvalue();
+
+    }
+  });
+}else{
+  this.OrderItemList.forEach(element => {
+    if(element.Id==id){
+element.Status="ReadyToServe";
+this.checkvalue();
+
+    }
+  });
+}
+  }
+
+  viewitems(item){
+    // this.ViewOrderItemList=[];
+    // this.ViewOrderItemList.push(item);
+    this.ViewOrderItemList=item;
+ console.log(this.ViewOrderItemList);
+  }
 }
